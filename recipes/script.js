32b10df1ad33e5938a4a93a056be51318bd8f86e
mjthurber <mjthurber@gmail.com>
2023-10-02 17:23:43 -0600

    Try #3

---
+++ recipes/script.js
@@ -0,0 +1,15 @@
+var submitButton = document.getElementById('submit-Button');
+
+function getApi() {
+  var requestUrl = 'https://api.edamam.com/api/recipes/v2?type=public&app_id=6ae8c73f&app_key=71480f0c50158929fad3cba79c8b94b4';
+
+  fetch(requestUrl)
+    .then(function (response)  {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    }
+    )};
+  
+  submitButton.addEventListener('click', getApi);
