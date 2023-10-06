da0d533109fd8a3c1d97e6096f522a0b9d8a1c8c
Maggie McCausland <maggiemccausland@hotmail.com>
2023-10-05 18:33:06 -0600

    recipes can now be saved to favorites

--- recipes/script.js
+++ recipes/script.js
@@ -46,6 +46,7 @@ function searchRecipes(query) {
 
 
 var tryArr = [];
+var favoriteArr = [];
 
 // Function to save a recipe
 function saveRecipe(image, label, cuisineType, dietLabels, url) {
@@ -94,12 +95,12 @@ function displaySavedTry() {
                   <div style="height: 150px">
                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${savedTries[i].label}</h5>
                       <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${savedTries[i].type},${savedTries[i].dietLabels}</p>
+                      <a href="${savedTries[i].recipeUrl}" target="_blank"><button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">See How To Cook</button></a>
                   </div>
             
                   <div>
-                      <a href="${savedTries[i].recipeUrl}" target="_blank"><button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                      See How To Cook</button></a>
-                       <button class="save-card inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick="favorteRecipe()">&#9829;</button>
+                       <button class="save-card inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick="favoriteRecipe('${savedTries[i].image}', '${savedTries[i].label}', '${savedTries[i].type}', '${savedTries[i].dietLabels}', '${savedTries[i].recipeUrl}')">&#9829;</button>
+                       <button class="remove-card inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Remove</button>
                   </div>
                 </div>
             </div>`
@@ -111,6 +112,71 @@ function displaySavedTry() {
   }
 };
 
+
+// Function to save Favorite recipe
+function favoriteRecipe(image, label, cuisineType, dietLabels, url) {
+  // TODO: Implement this function to save the recipe to the user's favorites or to try
+  // You can use the recipe URI as a unique identifier for the recipe
+
+  var storedFavorites = JSON.parse(localStorage.getItem("favorite"));
+
+  if (storedFavorites !== null) {
+    favoriteArr = storedFavorites;
+  };
+
+  localStorage.setItem("favorite", JSON.stringify(favoriteArr));
+
+  var recipeObj = {
+    "image": image,
+    "label": label,
+    "type": cuisineType,
+    "dietLabels": dietLabels,
+    "recipeUrl": url,
+  }
+  favoriteArr.push(recipeObj);
+  localStorage.setItem("favorite", JSON.stringify(favoriteArr));
+
+  displaySavedFavorites();
+}
+
+function displaySavedFavorites() {
+  var savedFavorites = JSON.parse(localStorage.getItem('favorite'));
+  var displaySavedFavorites = document.querySelector(".saved-favorites");
+
+  displaySavedFavorites.innerHTML = " ";
+
+
+  if (savedFavorites < 1) {
+    console.log("empty")
+
+  } else {
+    console.log(">>", savedFavorites)
+    for (var i = 0; i < savedFavorites.length; i++) {
+      console.log(savedFavorites[i].image)
+
+      const card = $(
+        ` <div class="recipe-card max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <img class="rounded-t-lg" style="width: 100%" src="${savedFavorites[i].image}" alt="${savedFavorites[i].label}" />
+              <div class="p-5">
+            
+                  <div style="height: 150px">
+                       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${savedFavorites[i].label}</h5>
+                      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${savedFavorites[i].type},${savedFavorites[i].dietLabels}</p>
+                      <a href="${savedFavorites[i].recipeUrl}" target="_blank"><button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">See How To Cook</button></a>
+                  </div>
+            
+                  <div>
+                       <button class="remove-card inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Remove</button>
+                  </div>
+                </div>
+            </div>`
+      );
+
+      $('.saved-favorites').append(card);
+
+    }
+  }
+};
 
 
 // CLICK EVENT FOR TOGGLE CONTENT
@@ -136,3 +202,4 @@ $('#submit-button').click(function () {
 
 
 displaySavedTry();
+displaySavedFavorites();
